7c163159990c10f1ced58b142bf789f89fbb1a45
Filipe Varela <filipe-varela@telecom.pt>
2012-08-30 09:17:50 +0100

    Removed unused old stuff

--- generator/build.sh
+++
@@ -1,1 +0,0 @@
-g++ generator.cpp noiseutils.cpp -I/usr/local/include -I. -o generator -lnoise
--- generator/genlib/buildlib.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#g++ -shared -Wl,soname,libgenlib.so -o libgenlib.so genlib.o noiseutils.o
-gcc -dynamiclib -flat_namespace -o genlib.dylib *.c
