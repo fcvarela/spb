9e6cc99415f0f435161f2a87453e8fadeedae086
Filipe Varela <filipe-varela@telecom.pt>
2012-10-09 15:59:30 +0100

    Reduced dependencies on dylibds

--- cocoa/build.sh
+++ cocoa/build.sh
@@ -16,9 +16,11 @@ tinythread/*.cpp \
 -framework OpenAL \
 -framework GLUT \
 /usr/local/lib/libconfig++.a \
--lftgl \
--lglew \
+/usr/local/lib/libGLEW.a \
+/usr/local/lib/libftgl.a \
 ./glfw/lib/cocoa/libglfw.a \
+-L/usr/X11/lib \
+-lfreetype \
 -o SPB.app/Contents/MacOS/spb
 
 cp -r conf spb.app/Contents/Resources
