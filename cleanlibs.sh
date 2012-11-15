7a46af7bac43d6563891503e70466d43624acfd8
Filipe Varela <fcvarela@gmail.com>
2012-11-15 15:32:48 +0000

    Now?

--- cleanlibs.sh
+++ cleanlibs.sh
@@ -2,9 +2,9 @@
 
 cd libs
 
-find . | grep .a$ | xargs rm
-find . | grep .o$ | xargs rm
-find . | grep .dylib | xargs rm
+find . | grep grep "[.]a$" | xargs rm
+find . | grep grep "[.]o$" | xargs rm
+find . | grep grep "[.]dylib$" | xargs rm
 
 cd ..
 rm -rf libs/libnoise/build
