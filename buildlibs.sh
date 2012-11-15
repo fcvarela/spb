27a89f9866727e370b965f30c1e7f9ab0d44b8d4
Filipe Varela <fcvarela@gmail.com>
2012-11-15 14:05:31 +0000

    Hopefuly goof

--- buildlibs.sh
+++ buildlibs.sh
@@ -1,23 +1,34 @@
-#!/bin/bash
+#!/bin/bash -ex
 
+echo "Will build libs"
 cd libs
 
+echo "libsoil..."
 cd libSOIL
 ./build.sh
 cd ..
+echo "done"
 
+echo "libtinythread..."
 cd libtinythread
 ./build.sh
 cd ..
+echo "done"
 
+echo "libcdf..."
 cd libcdf
 ./build.sh
 cd ..
+echo "done"
 
+echo "libnoise..."
 mkdir -p libnoise/build
 cd libnoise/build
 cmake ..
 make
 cd ..
+echo "done"
 
 cd ..
+echo "all built"
+
